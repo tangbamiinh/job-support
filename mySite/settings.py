1dc25b7058d6b8c8f72062468ea9973c0919216e
minhtb <97092893+minhtb-flinters@users.noreply.github.com>
2022-08-04 18:26:40 +0700

    update settings.py 3

--- mySite/settings.py
+++ mySite/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'jobs', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
